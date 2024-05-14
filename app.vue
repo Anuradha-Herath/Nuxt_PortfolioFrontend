dd15ef2b380a785509dab1f5daaffe54ef9f6251
unknown <anuradhaherath2001@gmail.com>
2024-05-14 21:05:58 +0530

    add blogs to app.vue template

--- app.vue
+++ app.vue
@@ -1,5 +1,43 @@
 <template>
   <div>
-    <div><h1>Anuradha</h1></div>
+    <h1>I am {{ name }}</h1>
+
+    <div v-if="pendingProjects">Loading projects...</div>
+    <div v-else>
+      <h2>Projects</h2>
+      <ul>
+        <li v-for="project in projects" :key="project.name">
+          <h3>{{ project.name }}</h3>
+          <p>{{ project.description }}</p>
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="pendingBlogs">Loading blogs...</div>
+    <div v-else>
+      <h2>Blogs</h2>
+      <ul>
+        <li v-for="blog in blogs" :key="blog.title">
+          <h3>{{ blog.title }}</h3>
+          <p>{{ blog.content }}</p>
+          <p>{{ blog.date }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import useFetch from './useFetch'; 
+
+  const name = "Anuradha";
+
+  const { data: projects, pending: pendingProjects, error: errorProjects } = useFetch("http://localhost:5000/projects");
+
+  const { data: blogs, pending: pendingBlogs, error: errorBlogs } = useFetch("http://localhost:5000/blogs");
+</script>
+
+
+<style scoped>
+</style>
