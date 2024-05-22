548fca1ee064a4378d7eb45000b21443229c2b84
Anurdha-Herath <anuradhahearath2001@gmail.com>
2024-05-22 20:43:08 +0530

    changed the code in app.vue

--- app.vue
+++ app.vue
@@ -1,43 +1,41 @@
 <template>
   <div>
-    <h1>I am {{ name }}</h1>
+    <h1>{{ name }}</h1>
 
-    <div v-if="pendingProjects">Loading projects...</div>
-    <div v-else>
-      <h2>Projects</h2>
-      <ul>
-        <li v-for="project in projects" :key="project.name">
-          <h3>{{ project.name }}</h3>
-          <p>{{ project.description }}</p>
-        </li>
-      </ul>
-    </div>
+    <div v-if="pending">Loading...</div>
 
-    <div v-if="pendingBlogs">Loading blogs...</div>
-    <div v-else>
-      <h2>Blogs</h2>
-      <ul>
-        <li v-for="blog in blogs" :key="blog.title">
-          <h3>{{ blog.title }}</h3>
-          <p>{{ blog.content }}</p>
-          <p>{{ blog.date }}</p>
-        </li>
-      </ul>
-    </div>
+    <h1>Projects</h1>
+
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
+      </li>
+    </ul>
+  </div>
+  <div>
+    <h1>Blogs</h1>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.title">
+        <h2>{{ blog.title }}</h2>
+        <p>{{ blog.content }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-  import { ref, onMounted } from 'vue';
-  import useFetch from './useFetch'; 
+const name = "Anuradha Herath";
+const {
+  data: projects,
+  pending: pendingProjects,
+  error: errorProjects,
+} = useFetch("http://localhost:5000/projects");
 
-  const name = "Anuradha";
+const {
+  data: blogs,
+  pending: pendingBlogs,
+  error: errorBlogs,
+} = useFetch("http://localhost:5000/blogs");
 
-  const { data: projects, pending: pendingProjects, error: errorProjects } = useFetch("http://localhost:5000/projects");
-
-  const { data: blogs, pending: pendingBlogs, error: errorBlogs } = useFetch("http://localhost:5000/blogs");
 </script>
-
-
-<style scoped>
-</style>
